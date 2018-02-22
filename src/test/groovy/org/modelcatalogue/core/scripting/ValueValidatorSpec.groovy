8a7f27459425294da4e0e63ca0f484cb7250e5e0
Sergio del Amo <sergio.delamo@softamo.com>
2018-02-22 14:21:14 +0100

    Add rule validation examples to test

--- src/test/groovy/org/modelcatalogue/core/scripting/ValueValidatorSpec.groovy
+++ src/test/groovy/org/modelcatalogue/core/scripting/ValueValidatorSpec.groovy
@@ -11,9 +11,11 @@ class ValueValidatorSpec extends Specification {
         expected == ValueValidator.validateRule(new ValidatingImpl(explicitRule: rule), value)
 
         where:
-        rule                                | value | expected
-        "x == null || x in ['red', 'blue']" | 're'  | false
-        "x == null || x in ['red', 'blue']" | 'red' | true
+        rule                                                                | value | expected
+        "x == null || x in ['red', 'blue']"                                 | 're'  | false
+        "x == null || x in ['red', 'blue']"                                 | 'red' | true
+        "x == null || x in ['M0', 'M1', 'M1a', 'M1b', 'M1c', 'Unknown']"    | 'M1b' | true
+        "x == null || x in ['M0', 'M1', 'M1a ', 'M1b ', 'M1c', 'Unknown']"  | 'M1b' | false
         description = expected ? 'is valid' : 'is not valid'
     }
 }
