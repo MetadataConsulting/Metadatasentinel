153a8ef6a8db45dab24567c6ea2fbfb87c193b4c
James Dai <james.dai.uk@googlemail.com>
2018-09-03 11:19:11 +0100

    simplified RecordControllerSpec

--- src/test/groovy/uk/co/metadataconsulting/sentinel/RecordControllerSpec.groovy
+++ src/test/groovy/uk/co/metadataconsulting/sentinel/RecordControllerSpec.groovy
@@ -18,7 +18,7 @@ class RecordControllerSpec extends Specification implements ControllerUnitTest<R
         view == '/record/index'
     }
 
-    def "RecordController.index model contains recordCollectionId"() {
+    def "RecordController.index model contains fields recordCollectionId, invalidRecordTotal, allRecordTotal, recordList, paginationQuery"() {
         given:
         controller.recordService = Mock(RecordService)
         controller.recordCollectionGormService = Mock(RecordCollectionGormService)
@@ -31,74 +31,13 @@ class RecordControllerSpec extends Specification implements ControllerUnitTest<R
         then:
         response.status == SC_OK
         model
-        model.containsKey('recordCollectionId')
+        model.containsKey(field)
+
+        where:
+        field << ['recordCollectionId', 'invalidRecordTotal','allRecordTotal', 'recordList', 'paginationQuery' ]
     }
 
-    def "RecordController.index model contains invalidRecordTotal"() {
-        given:
-        controller.recordService = Mock(RecordService)
-        controller.recordCollectionGormService = Mock(RecordCollectionGormService)
-
-        when:
-        params.recordCollectionId = 1
-        request.method = 'GET'
-        Map model = controller.index()
-
-        then:
-        response.status == SC_OK
-        model
-        model.containsKey('invalidRecordTotal')
-    }
-
-    def "RecordController.index model contains allRecordTotal"() {
-        given:
-        controller.recordService = Mock(RecordService)
-        controller.recordCollectionGormService = Mock(RecordCollectionGormService)
-
-        when:
-        params.recordCollectionId = 1
-        request.method = 'GET'
-        Map model = controller.index()
-
-        then:
-        response.status == SC_OK
-        model
-        model.containsKey('allRecordTotal')
-    }
-
-    def "RecordController.index model contains recordList"() {
-        given:
-        controller.recordService = Mock(RecordService)
-        controller.recordCollectionGormService = Mock(RecordCollectionGormService)
-
-        when:
-        params.recordCollectionId = 1
-        request.method = 'GET'
-        Map model = controller.index()
-
-        then:
-        response.status == SC_OK
-        model
-        model.containsKey('recordList')
-    }
-
-    def "RecordController.index model contains paginationQuery"() {
-        given:
-        controller.recordService = Mock(RecordService)
-        controller.recordCollectionGormService = Mock(RecordCollectionGormService)
-
-        when:
-        params.recordCollectionId = 1
-        request.method = 'GET'
-        Map model = controller.index()
-
-        then:
-        response.status == SC_OK
-        model
-        model.containsKey('paginationQuery')
-    }
-
-    def "RecordController.recordTotal model contains paginationQuery"() {
+    def "RecordController.recordTotal model contains field paginationQuery"() {
         given:
         controller.recordService = Mock(RecordService)
         controller.recordCollectionGormService = Mock(RecordCollectionGormService)
@@ -114,7 +53,7 @@ class RecordControllerSpec extends Specification implements ControllerUnitTest<R
         model.containsKey('recordTotal')
     }
 
-    def "RecordController.show model contains recordPortionList"() {
+    def "RecordController.show model contains fields recordPortionList, recordId, recordPortionTotal"() {
         given:
         controller.recordPortionGormService = Mock(RecordPortionGormService)
         controller.recordCollectionMappingGormService = Mock(RecordCollectionMappingGormService)
@@ -128,42 +67,12 @@ class RecordControllerSpec extends Specification implements ControllerUnitTest<R
         then:
         response.status == SC_OK
         model
-        model.containsKey('recordPortionList')
+        model.containsKey(field)
+
+        where:
+        field << ['recordPortionList', 'recordId', 'recordPortionTotal']
     }
 
-    def "RecordController.show model contains recordId"() {
-        given:
-        controller.recordPortionGormService = Mock(RecordPortionGormService)
-        controller.recordCollectionMappingGormService = Mock(RecordCollectionMappingGormService)
-        controller.recordCollectionGormService = Mock(RecordCollectionGormService)
-
-        when:
-        params.recordId = 1
-        request.method = 'GET'
-        Map model = controller.show()
-
-        then:
-        response.status == SC_OK
-        model
-        model.containsKey('recordId')
-    }
-
-    def "RecordController.show model contains recordPortionTotal"() {
-        given:
-        controller.recordPortionGormService = Mock(RecordPortionGormService)
-        controller.recordCollectionMappingGormService = Mock(RecordCollectionMappingGormService)
-        controller.recordCollectionGormService = Mock(RecordCollectionGormService)
-
-        when:
-        params.recordId = 1
-        request.method = 'GET'
-        Map model = controller.show()
-
-        then:
-        response.status == SC_OK
-        model
-        model.containsKey('recordPortionTotal')
-    }
 
     def "RecordController.index model contains recordCollectionEntity"() {
         given:
