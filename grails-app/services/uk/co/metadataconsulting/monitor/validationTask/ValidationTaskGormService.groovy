cd29fdabef65fedea5c3bb0b0e2d92a3e9c44786
Sergio del Amo <sergio.delamo@softamo.com>
2021-10-01 17:09:20 +0200

    fix Validation Task deletion

--- grails-app/services/uk/co/metadataconsulting/monitor/validationTask/ValidationTaskGormService.groovy
+++ grails-app/services/uk/co/metadataconsulting/monitor/validationTask/ValidationTaskGormService.groovy
@@ -2,6 +2,7 @@ package uk.co.metadataconsulting.monitor.validationTask
 
 import grails.gorm.DetachedCriteria
 import grails.gorm.transactions.ReadOnly
+import grails.gorm.transactions.Transactional
 import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
@@ -9,34 +10,26 @@ import groovy.util.logging.Slf4j
 
 @Slf4j
 @CompileStatic
-class ValidationTaskGormService {
-//    /**
-//     * Cascading delete
-//     * @param validationTaskId
-//     */
-//    void delete(Long validationTaskId) {
-//        ValidationTask validationTask = getValidationTask(validationTaskId)
-//        queryByRecordCollection(validationTask).list().each {it.delete(flush:true)}
-//        validationTask.delete(flush:true)
-//
-//    }
-//
+class ValidationTaskGormService  implements GormErrorsMessage {
+    @Transactional
+    void delete(ValidationTask validationTask) {
+        validationTask.delete()
+    }
+
     @ReadOnly
     ValidationTask getValidationTask(Long validationTaskId) {
         return ValidationTask.get(validationTaskId)
     }
 
     @CompileDynamic
-    DetachedCriteria<ValidationPass> queryByRecordCollection(ValidationTask validationTaskParam) {
-       return ValidationPass.where { validationTask == validationTaskParam }
+    DetachedCriteria<ValidationPass> queryByRecordCollection(ValidationTask validationTaskParam) {       return ValidationPass.where { validationTask == validationTaskParam }
     }
 //
 //    /**
 //     * Get Validation Passes for this Validation Task
 //     * @param validationTaskId
 //     * @return
-//     */
-//    List<ValidationPass> getValidationPasses(Long validationTaskId) {
+//     *///    List<ValidationPass> getValidationPasses(Long validationTaskId) {
 //        return queryByRecordCollection(getValidationTask(validationTaskId)).list()
 //    }
 //
@@ -44,7 +37,6 @@ class ValidationTaskGormService {
     Long countValidationPasses(Long validationTaskId) {
         return queryByRecordCollection(getValidationTask(validationTaskId)).count() as Long
     }
-
     @ReadOnly
     List<ValidationTask> findAll(Map args) {
         ValidationTask.where {}.list(args)
